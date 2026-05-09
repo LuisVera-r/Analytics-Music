e4f9b168143181ec39069c2df6ef9f4c539b43cc
Luis Vera <luisalbertoverat7@gmail.com>
2026-05-09 14:32:58 -0600

    Import Faker for generating fake artist data
    Added Faker library to generate fake data for artists.

--- data/generators/dim_artist_generator.py
+++ data/generators/dim_artist_generator.py
@@ -1,6 +1,7 @@
 import random
 from faker import Faker
 
+fake = Faker()
 def generate_dim_artist(conn):
   genres = ['Pop', 'Rock', 'Hip-Hop', 'Electronic', 'Jazz',
           'Classical', 'Reggaeton', 'R&B', 'Latin', 'Metal','Indie']
